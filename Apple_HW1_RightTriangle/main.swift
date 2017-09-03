dac392d7cd5217ec542d836c26b40339c7f37a91
errornosignal <errornosignal@users.noreply.github.com>
2017-09-03 09:30:57 -0500

    Initial Commit

---
+++ Apple_HW1_RightTriangle/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Apple_HW1_RightTriangle
+//
+//  Created by Reid Nolan on 9/3/17.
+//  Copyright © 2017 Reid Nolan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
