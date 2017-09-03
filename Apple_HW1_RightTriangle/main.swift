fa2b24f27ebb06dc2803b58ee6c491e280e484ed
errornosignal <errornosignal@users.noreply.github.com>
2017-09-03 10:08:49 -0500

    9/3/17 10:08am

--- Apple_HW1_RightTriangle/main.swift
+++ Apple_HW1_RightTriangle/main.swift
@@ -8,5 +8,32 @@
 
 import Foundation
 
-print("Hello, World!")
+var side1, side2, side3: Double
+side1 = 0.0
+side2 = 0.0
+side3 = 0.0
+
+var inputString = ""
+
+print("HW1-RightTriangle\n");
+
+print("Enter length of side 1: ")
+inputString = readLine()!
+side1 = Double(inputString)!
+
+print("Enter length of side 2: ")
+inputString = readLine()!
+side2 = Double(inputString)!
+
+print("Enter length of side 3: ")
+inputString = readLine()!
+side3 = Double(inputString)!
+
+
+print("side1 = \(String(describing: side1))")
+print("side2 = \(String(describing: side2))")
+print("side3 = \(String(describing: side3))")
+
+
+
 
