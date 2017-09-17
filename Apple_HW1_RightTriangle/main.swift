afdc6c4ce5472de3767f0eb7465f2509b0cd179a
errornosignal <errornosignal@users.noreply.github.com>
2017-09-16 22:09:48 -0500

    Final (working)

--- Apple_HW1_RightTriangle/main.swift
+++ Apple_HW1_RightTriangle/main.swift
@@ -24,37 +24,35 @@ func validateDoubleInput(prompt: String) -> Double {
                 return side;
             }
             else {
-                print("Error! Minimum value is 0.1")
+                print("Error! Input is below '0.1' minimum value.")
             }
         }
         else {
-            print("Error! Input is not a double value")
+            print("Error! Input is not a valid double value.")
         }
     }
 }
 
 //set user input to variables
-let side1 = validateDoubleInput(prompt: "Enter length of side 1: ")
-let side2 = validateDoubleInput(prompt: "Enter length of side 2: ")
-let side3 = validateDoubleInput(prompt: "Enter length of side 3: ")
+let side1 = validateDoubleInput(prompt: "Enter length of side 1:")
+let side2 = validateDoubleInput(prompt: "Enter length of side 2:")
+let side3 = validateDoubleInput(prompt: "Enter length of side 3:")
 
-//add variables to array and sort
+//add variables to array
 var sidesArray = [side1, side2, side3]
+
+//sort values in array
 sidesArray.sort()
 
-//perform mathematical computations
-let side1Sqrd = pow(sidesArray[0],2)
-let side2Sqrd = pow(sidesArray[1],2)
-let side3Sqrd = pow(sidesArray[2],2)
-let actualHypotenuse = sqrt(side1Sqrd + side2Sqrd)
+//display side lengths after sort
+print("\nSides(ordered) =", sidesArray)
+print("Apparent Hypotenuse = \(String(describing: sidesArray[2]))") //largest side value in array
 
-//output all values to console
-print("\n")
-print("side1 = \(String(describing: sidesArray[0]))")
-print("side2 = \(String(describing: sidesArray[1]))")
-print("side3 = \(String(describing: sidesArray[2]))")
-print("apparentHypotenuse = \(String(describing: sidesArray[2]))")
-print("actualHypotenuse = \(String(describing: actualHypotenuse))")
+//perform pythagoream theorem calculations (a^2+b^2=c^2)
+let side1Sqrd = pow(sidesArray[0],2) //a^2
+let side2Sqrd = pow(sidesArray[1],2) //b^2
+let side3Sqrd = pow(sidesArray[2],2) //c^2
+print("Actual Hypotenuse", sqrt(side1Sqrd + side2Sqrd)) //sqrt(a^2+b^2)
 
 //confirm triangle type *INCLUDES BONUS*
 if side1Sqrd + side2Sqrd == side3Sqrd {
@@ -69,6 +67,5 @@ else if side1 == side2 || side1 == side3 || side2 == side3 {
 else if side1 != side2 && side1 != side3 && side2 != side3 {
     print("Triangle is SCALENE\n")
 }
-else {
-    //do nothing
-}
+else {/*do nothing*/}
+//END OF PROGRAM
